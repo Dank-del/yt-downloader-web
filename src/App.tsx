028c5b0932fa89a06349f5959a7c19e9b6971569
Sayan Biswas <sayan@pokurt.me>
2022-12-16 01:01:49 +0530

    use new route
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,42 @@ function App() {
   const [url, setUrl] = useState('');
   const [alertDiv, setAlert] = useState(<div></div>);
   const [formats, setFormats] = useState<any[]>([]);
+
+  // const mergeAudioAndVideo = (videoUrl: any, audioUrl: any) => {
+  //   const videoElement: any = document.createElement('video');
+  //   videoElement.src = URL.createObjectURL(videoUrl);
+  //   document.body.appendChild(videoElement);
+
+  //   const audioElement: any = document.createElement('audio');
+  //   audioElement.src = URL.createObjectURL(audioUrl);
+  //   document.body.appendChild(audioElement);
+
+  //   // Merge the audio and video streams
+  //   const mediaStream = new MediaStream();
+  //   mediaStream.addTrack(videoElement.captureStream().getVideoTracks()[0]);
+  //   mediaStream.addTrack(audioElement.captureStream().getAudioTracks()[0]);
+
+  //   const mergedVideoElement = document.createElement('video');
+  //   mergedVideoElement.srcObject = mediaStream;
+  //   document.body.appendChild(mergedVideoElement);
+  //   return mergedVideoElement;
+  // }
+
   const onClick = async () => {
     try {
       const res = await fetch(
-        `${import.meta.env.VITE_API_URL}/download/video?video_url=${encodeURIComponent(url)}`
+        `${import.meta.env.VITE_API_URL}/download/ssyoutube?video_url=${encodeURIComponent(url)}`
       )
       if (res.ok) {
         const jsn = await res.json();
         setAlert(
           <NextUiCard style={{ width: 'auto', margin: '2rem' }}>
-          <NextUiCard.Body>
-            <Text h3>Video: {jsn.title}</Text>
-          </NextUiCard.Body>
-        </NextUiCard>
+            <NextUiCard.Body>
+              <Text h3>Video: {jsn.meta.title}</Text>
+            </NextUiCard.Body>
+          </NextUiCard>
         )
-        setFormats([...jsn.formats].reverse());
+        setFormats([...jsn.url].reverse());
         return
       }
     } catch (err) {
@@ -75,7 +96,7 @@ function App() {
           <Grid.Container gap={2} justify="center">
             {formats.map(data => {
               return <Grid xs={12} md={3}>
-                <Card key={data.ext} ext={data.ext} format={data.format} format_note={data.format_note} container={data.container} url={data.url} />
+                <Card key={data.ext} ext={data.ext} format={data.name} format_note={data.quality} container={data.attr.title} url={data.url} />
               </Grid>
             })}
           </Grid.Container>
